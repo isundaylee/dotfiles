7b57b114c40ab74f094963d0e5178712c73d055c
Jiahao Li <isundaylee.reg@gmail.com>
2014-08-09 22:10:36 +0800

    Adds the path of balance_meter to $PATH.

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-export PATH="$HOME/Programming/Git/sunutils:$PATH"
+export PATH="$HOME/Programming/Git/sunutils:$HOME/Programming/Git/balance_meter:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
