281dfe2dc1c06dcc9c71235350e47e79ddab23b7
Jiahao Li <isundaylee.reg@gmail.com>
2014-12-16 13:31:15 -0500

    Some Django aliases.

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,2 +1,7 @@
 alias da='django-admin'
 alias pm='python manage.py'
+
+alias epip='venv/bin/pip'
+alias ep='venv/bin/python'
+alias ea='source venv/bin/activate'
+alias pf='pip freeze > requirements.txt'
