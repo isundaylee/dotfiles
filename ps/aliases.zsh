0e57c538adf15c452d510b29e87f8736d94807eb
Jiahao Li <jiahao.li@purestorage.com>
2015-05-28 23:38:02 -0700

    Pure-related aliases.

--- ps/aliases.zsh
+++ ps/aliases.zsh
@@ -2,6 +2,10 @@ alias purity='export SKIP_GIT_PROMPT=true; c vm/home/jiahao/work/purity'
 alias vmmount='c && sshfs vm:/ vm'
 alias vmbr='vmbuildrun'
 
+
+alias build="cd $HOME/work/purity && make -j 4 CC=\"distcc gcc\" -C ../bld_linux/purity"
+alias lbuild="cd $HOME/work/purity && make -j 4 -C ../bld_linux/purity"
+
 function vmbuild() {
   if (( $# == 0 )) then
     ssh vm ". ~/.bash_profile && build"
