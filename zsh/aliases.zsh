6481441b0720a7bb41af18167331c8a1ece65ee1
Jiahao Li <jiahao.li@purestorage.com>
2015-06-12 11:34:41 -0700

    search

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,16 @@ alias reload!='. ~/.zshrc'
 alias 1803='class 1803'
 alias 6006='class 6006'
 
+function search() {
+    DIR=.
+
+    if [ ! -z $2 ]; then
+        DIR=$2
+    fi
+
+    grep -r $1 $DIR
+}
+
 function class() {
   cd "`find_class $1`"
 }
