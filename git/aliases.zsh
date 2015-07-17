b794a34511f9847b74c554a401dbdb9cf0df8207
Jiahao Li <jiahao.li@purestorage.com>
2015-07-17 10:47:00 -0700

    Git one line log alias.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ fi
 
 # Utilities
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glol="git log --oneline"
 alias gd='git diff'
 alias gds='git diff --staged'
 alias gc='git commit'
