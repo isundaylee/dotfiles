febfde8fa8737c53e0be5a4676e8725e891c23af
Jiahao Li <isundaylee.reg@gmail.com>
2015-07-23 22:27:52 -0700

    Better vssh (vagrant ssh).

--- vagrant/aliases.zsh
+++ vagrant/aliases.zsh
@@ -1,3 +1,18 @@
+alias vssh-config='vagrant ssh-config'
 alias viu='vagrant init hashicorp/precise32'
 alias vup='vagrant up'
-alias vssh='vagrant ssh'
+
+VAGRANT_SSH_CONFIG_FILE='.vagrant_ssh_config'
+
+function vssh() {
+    if [[ $1 == '-f' ]]; then
+        rm $VAGRANT_SSH_CONFIG_FILE > /dev/null
+    fi
+
+    if [[ ! -f $VAGRANT_SSH_CONFIG_FILE ]]; then
+        echo "Generating $VAGRANT_SSH_CONFIG_FILE"
+        vagrant ssh-config > $VAGRANT_SSH_CONFIG_FILE
+    fi
+
+    ssh -F $VAGRANT_SSH_CONFIG_FILE default
+}
